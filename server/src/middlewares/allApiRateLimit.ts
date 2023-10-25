e1b32345f4c515764393fa3d3389c08a36fc16d7
yuri2 <yuri2peter@qq.com>
2023-10-25 18:15:06 +0800

    v1.2.2

--- server/src/middlewares/allApiRateLimit.ts
+++ server/src/middlewares/allApiRateLimit.ts
@@ -5,9 +5,9 @@ import { startsWith } from '../utils/miscs';
 const checkList = ['/api'];
 const excludeList: string[] = [];
 
-// 十分钟内只允许访问9999次
+// 十分钟内只允许访问99999次
 const apiRateLimiter = new RateLimiter({
-  max: 9999,
+  max: 99999,
   windowSeconds: 10 * 60,
 });
 
